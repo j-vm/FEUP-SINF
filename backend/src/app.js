35127b8e391d7177e075ecb171c82bbb15175573
João N. Matos <me@joaonmatos.com>
2020-11-22 23:35:12 +0000

    feat: add backend and compose

---
+++ backend/src/app.js
@@ -0,0 +1,38 @@
+const isDev = require("isdev");
+if (isDev) {
+  const dotenv = require("dotenv");
+  dotenv.config();
+}
+
+const { APP_PORT, JWT_SECRET } = process.env;
+if (!JWT_SECRET) {
+  console.error("ERROR: JWT_SECRET variable must be set");
+  process.exit(1);
+}
+
+const koa = require("koa");
+const app = new koa();
+
+const bodyParser = require("koa-bodyparser");
+app.use(bodyParser());
+
+// Custom 401 handling if you don't want to expose koa-jwt errors to users
+app.use(function (ctx, next) {
+  return next().catch((err) => {
+    console.log(`INFO: caught unauthorized access: ${err}`);
+    if (401 == err.status) {
+      ctx.status = 401;
+      ctx.body = "Protected resource, use Authorization header to get access\n";
+    } else {
+      throw err;
+    }
+  });
+});
+
+const jwt = require("koa-jwt");
+app.use(jwt({ secret: JWT_SECRET }).unless({ path: /^\/login$/ }));
+
+const router = require("./router");
+app.use(router.routes());
+
+app.listen(APP_PORT || 3000);
---
+++ backend/src/db/migrations/20201122151352-create-user.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      hashed_password: {
+        type: Sequelize.TEXT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
