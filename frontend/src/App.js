5ad52a2166b8966e6e456dad7ce9c451e0cb3876
João N. Matos <me@joaonmatos.com>
2020-11-22 22:33:22 +0000

    fix(frontend): make it actually work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
-import React from 'react'; 
+import React from "react";
 //import NavBar from './navBar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Home} from './Home';
-import {About} from './About';
-import {Services} from './Services';
-import {NoMatch} from './NoMatch';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Services } from "./Services";
+import { NoMatch } from "./NoMatch";
 
-class App extends Component {
-  render() {
-    return (
-      <ReactFragment>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/services" component={Services} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Router>
-      </ReactFragment>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
