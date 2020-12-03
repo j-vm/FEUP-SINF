42a765d16490b2bcce9bbca29e5ee763bf7f3e6f
João Nuno Matos <me@joaonmatos.com>
2020-12-03 17:51:49 +0000

    frontend: implement auth

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,71 +1,25 @@
-import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
-import { Button, Form, FormGroup, Label, Input, Alert} from "reactstrap";
-
+import React from "react";
+import { Jumbotron, Container } from "reactstrap";
+import { Link } from "react-router-dom";
+import { Layout } from "../components/Layout";
 
 export const Home = () => {
-	return (
-		<div className="mt-5">
-			<Jumbotron fluid>
-				<Container className="text-info" fluid style={{ textAlign: 'center' }}>
-					<h1 className="display-3" >Welcome to InterCompany!</h1>
-				</Container>
-			</Jumbotron>
-			<Report></Report>	
-		</div>
-	)
-}
-
-const Report = () => {
-	return(
-		<div>
-			 <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "30vh"
-        }}
-      >
-        <Form className="w-25">
-        Sign in 
-
-          <FormGroup class="mt-2">
-            <Label for="username" hidden>
-              Email
-            </Label>
-            <Input
-              type="username"
-              name="userName"
-              id="username"
-              placeholder="New Username"
-            />
-          </FormGroup>{" "}
-          <FormGroup>
-            <Label for="examplePassword" hidden>
-              Password
-            </Label>
-            <Input
-              type="password"
-              name="password"
-              id="examplePassword"
-              placeholder="New Password"
-            />
-          </FormGroup>{" "}
-          <Button
-            onClick={event => (window.location.href = "/info/")}
-            color="success"
+  return (
+    <Layout>
+      <div className="mt-5">
+        <Jumbotron fluid>
+          <Container
+            className="text-info"
+            fluid
+            style={{ textAlign: "center" }}
           >
-            Submit
-          </Button>{" "}
-			<div class="mt-5">
-				<Alert color="danger">
-				New here?
-				<a href="/setup/" className="alert-link">{' '}Create an account!</a>
-				</Alert>
-		  	</div>
-        </Form>
+            <h1 className="display-3">Welcome to InterCompany!</h1>
+          </Container>
+        </Jumbotron>
+        <Link to="/login" className="btn btn-primary">
+          Log In
+        </Link>
       </div>
-		</div>
-	)
-}
+    </Layout>
+  );
+};
