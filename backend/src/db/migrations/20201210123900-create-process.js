d56baee813cfeca442b4d3b10542c1144f12648f
João Nuno Matos <me@joaonmatos.com>
2020-12-10 13:05:07 +0000

    feat(backend): add Process & ProcessStep db models

---
+++ backend/src/db/migrations/20201210123900-create-process.js
@@ -0,0 +1,27 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Processes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Processes");
+  },
+};
