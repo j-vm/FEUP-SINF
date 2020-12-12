79e4da9861403291edc8bec4838d7023574873f6
j-vm <j-vm@LAPTOP-MT88LSVO.localdomain>
2020-12-12 04:12:30 +0000

    Execution logic done, connected to processes/test endpoint. Missing jasmin

--- backend/src/execution/executer.js
+++ backend/src/execution/executer.js
@@ -1,12 +1,48 @@
 const models = require("../jasmin/models");
+const fs = require("fs");
 
 module.exports = {
-  runExecutions: function (executions, processes) {
-    console.log(executions);
-    //executions.forEach(execution => {
-    //    console.log(execution)
-    //});
+  runExecutions: async function (executions, stepsToRun) {
+    const iterateStep = await Promise.all(
+      executions.map(async (execution, index) => {
+        if (stepsToRun[index] == null) return 0;
+        return await runStep(execution, stepsToRun[index]);
+      })
+    );
   },
 };
 
-function runStep(ProcessStep) {}
+// return 1 if successful
+function runStep(exec, step) {
+  console.log(step), console.log(exec);
+  let returnCode = 0;
+  switch (step.documentType) {
+    case "supplierInvoice":
+      returnCode = handleSupplierInvoice(step.type, exec.id);
+      break;
+    case "docB":
+      //TODO: returnCode = docB(step.type, exec.id)
+      break;
+    case "docC":
+      //TODO: returnCode = docC(step.type, exec.id)
+      break;
+
+    default:
+      console.log("ERROR: Unrecognized Document type " + step.documentType);
+      return 1;
+  }
+  return returnCode;
+}
+
+function handleSupplierInvoice(type, id) {
+  if (type == "emit") {
+    //GET JSON DATA FROM FILE WITH FS
+    //Access Correct endpoint with the json data
+    console.log("Emiting Supplier Invoice");
+  } else if (type == "wait") {
+    //Access Correct endpoint to get the json data
+    //STORE JSON DATA ON FILE WITH FS
+    console.log("Looking for Supplier Invoice");
+  }
+  console.log("ERROR: Unrecognized step type " + type);
+}
