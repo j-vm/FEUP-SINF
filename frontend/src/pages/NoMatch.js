deb658c487a2fbe0e073284759e1c55790142738
Marta Lobo <56152625+martandradelobo@users.noreply.github.com>
2020-12-14 19:41:58 +0000

    frontend: about page, 404 not found, added logos

--- frontend/src/pages/NoMatch.js
+++ frontend/src/pages/NoMatch.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Media } from "reactstrap";
+import nomatch from "../404.png";
+import ReactLogo from "../besinflogo.png";
+
 
 export const NoMatch = () => {
   return (
     <div>
-      <h2>No Match</h2>
+       <div class="text-center" >
+        <Media middle href="#" >
+          <Media object src={ReactLogo}  height="400" alt="logo" />
+        </Media>
+        </div>
+      <div class="text-center" >
+        <Media middle href="#" >
+          <Media object src={nomatch}  height="400" alt="logo" />
+        </Media>
+        </div>
     </div>
   );
 };
